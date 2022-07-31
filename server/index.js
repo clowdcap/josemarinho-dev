7d77d2275a93e321b5c43e0095bab1afc76056e6
José Marinho <clowdcap@hotmail.com>
2022-07-31 14:13:06 -0300

    Update CRUD

--- server/index.js
+++ server/index.js
@@ -1,21 +1,61 @@
-var express = require('express');
+/* Importando bibliotecas e frameworks */
+var express = require('express')
 var cors = require('cors')
-var app = express();
 
+/* Instanciando Framework App */
+var app = express()
+
+/* Introduzidno cors e expressando o framework em json */
 app.use(cors())
+app.use(express.json())
 
-app.get('/', function(req, res) {
-    res.send('Olá Mundo!');
-  });
+/* Instanciando Nomes */
+const nomesTestes = ['Pessoa 1', 'Pessoa 2', 'Pessoa 3']
 
-/* 
-Ligando o servidor
-*/
+/* Pagina Incial */
+app.get('/', (req, res) => {
+    res.send('Olá Mundo!')
+})
+
+/* Listar nome pelo ID do array nomesTestes */
+app.get('/nomes/:index', (req, res) => {
+  const { index } = req.params
+  return res.json(nomesTestes[index])
+})
+
+/* Listando todos os nomes */
+app.get('/nomes', (req, res) => {
+  return res.json(nomesTestes)
+})
+
+/* Criando um novo nome */
+app.post('/nomes', (req, res) => {
+  const {nome} = req.body
+  nomesTestes.push(nome)
+  return res.json(nomesTestes)
+})
+
+/* Atualizado nomes */
+app.put('/nomes/:index', (req, res) => {
+  const {index} = req.params
+  const {nome} = req.body
+  nomesTestes[index] = nome
+  return res.json(nomesTestes)
+})
+
+/* Deletar nome */
+app.delete('/nomes/:index', (req, res) => {
+  const {index} = req.params
+  nomesTestes.splice(index, 1)
+  return res.json({ message: 'O nome foi deletado' })
+})
+
+/* Ligando o servidor */
 const porta = 3000
 try {
-    app.listen(porta, function() {
-        console.log(`Node Js::: Servidor conectado::: http://localhost:${porta}`);
-      });
+  app.listen(porta, function() {
+    console.log(`Node Js::: Servidor conectado::: http://localhost:${porta}`)
+  });
 } catch (error) {
-    console.log(error.message)
+  console.log(error.message)
 }
