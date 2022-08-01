14c108ab4a68df84f309a121f5217e73f33eb229
José Marinho <clowdcap@hotmail.com>
2022-08-01 13:07:35 -0300

    update ajustes

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import './app.css'
+import './App.css'
 
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 
 import Home from './pages/home/Home'
+
+// import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
