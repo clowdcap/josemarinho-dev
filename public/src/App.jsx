1ea6c748efeefd71ccdb738ff2aa710099c47ef2
José Marinho <clowdcap@hotmail.com>
2022-08-01 13:48:33 -0300

    update setup init from react

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './App.css'
 
+// Rotas
+// import { BrowserRouter } from 'react-router-dom'
+
+// Componentes Fixos
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 
+// Pages
 import Home from './pages/home/Home'
-
-// import { BrowserRouter } from 'react-router-dom'
+// import Contact from './pages/contact/Contact'
 
 const App = () => {
   return (
     <>
         <Header />
+
         <Home />
+        
         <Footer />
     </>
   )
--- public/src/pages/home/Home.jsx
+++ public/src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './home.css'
 
-import Capa from '../../components/capa/Capa'
+/* Componentes */
+import Capa from './../../components/capa/Capa'
+import Contato from './../../components/contato/Contato'
+import Sobre from './../../components/sobre/Sobre'
+import Newsletter from './../../components/newsletter/Newsletter'
+import Projetos from './../../components/projetos/Projetos'
+import Servicos from './../../components/servicos/Servicos'
 
 
 const Home = () => {
   return (
     <>
         <Capa />
+
     </>
   )
 }
