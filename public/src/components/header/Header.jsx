b848b0705d4265bb984dde779c3e6892d1b784ff
José Marinho <clowdcap@hotmail.com>
2022-08-01 15:56:02 -0300

    update setup

--- public/src/components/header/Header.jsx
+++ public/src/components/header/Header.jsx
@@ -5,21 +5,38 @@ const Header = () => {
   return (
     <header>
       <div className="container">
-        <div className="logo"><h3>Jose.dev</h3></div>
+        <div className="logo"><h3><a href="#home">Jose.dev</a></h3></div>
         <div className="menu">
 
           <div className="desktop">
             <ul>
               <li><a href="#home">Inicio</a></li>
               <li><a href="#sobre">Sobre</a></li>
+              <li><a href="#sobre">Serviços</a></li>
+              <li><a href="#sobre">Skills</a></li>
+              <li><a href="#contato">Contato</a></li>
+            </ul>
+          </div>
+
+          <div className="mobile">
+            Icone
+          </div>
+
+          <div className="mobile">
+            <ul>
+              <li><a href="#home">Inicio</a></li>
+              <li><a href="#sobre">Sobre</a></li>
+              <li><a href="#sobre">Serviços</a></li>
+              <li><a href="#sobre">Skills</a></li>
               <li><a href="#contato">Contato</a></li>
             </ul>
           </div>
           
+          
         </div>
       </div>
     </header>
-  ) 
+  )
 }
 
 export default Header
