924c7130513c3ded6483143af3c7787f8545725f
José Marinho <clowdcap@hotmail.com>
2022-08-01 14:50:25 -0300

    add style perform

--- public/src/components/header/Header.jsx
+++ public/src/components/header/Header.jsx
@@ -5,15 +5,15 @@ const Header = () => {
   return (
     <header>
       <div className="container">
-        <div className="logo"><h3>Logo</h3></div>
+        <div className="logo"><h3>Jose.dev</h3></div>
         <div className="menu">
+
           <div className="desktop">
             <ul>
-              <li><a href="#home">Home</a></li>
+              <li><a href="#home">Inicio</a></li>
               <li><a href="#sobre">Sobre</a></li>
               <li><a href="#contato">Contato</a></li>
             </ul>
-
           </div>
           
         </div>
